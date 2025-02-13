8d1873f279b5ea52ef6a40adf2c1132bb85d457a
vkantchev <vkantchev@users.noreply.github.com>
2025-02-13 08:39:02 -0800

    Bump rust to 1.84.1

--- configure.sh
+++ configure.sh
@@ -3,6 +3,11 @@
 echo 'Sourcing $HOME/.cargo/env ...'
 
 source $HOME/.cargo/env
+rustup override set 1.84.1
+
+echo 'Using rust version ...'
+rustc --version
+cargo --version
 
 echo 'Installing tools ...'
 cargo install diesel_cli --no-default-features --features sqlite
